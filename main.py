3b621eebfdad392145d1dcafb7aa74374c76abe5
TheYafen <theyafen@gmail.com>
2026-05-15 12:59:32 +0300

    add uniq func

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import os
 
+full_list = os.listdir("test/")
+
+def get_uniq_names(full_file_list):
+    uniq_names = set()
+    extensions = set()
+    for full_name in full_file_list:
+       u_name, ext = full_name.split('.')
+       uniq_names.add(u_name)
+       extensions.add(ext)
+    return uniq_names, extensions
+
+a, b = get_uniq_names(full_list)
+
+print(a)
+print(b)
